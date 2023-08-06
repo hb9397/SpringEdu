11aec550c85ddca7007a2e2047830f8758087b2d
hb9397 <hb9397@naver.com>
2023-08-06 19:14:09 +0900

    스프링입문_Section5_회원관리예제
    1. 회원 웹 기능 - 홈 화면 추가
    
    https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81-%EC%9E%85%EB%AC%B8-%EC%8A%A4%ED%94%84%EB%A7%81%EB%B6%80%ED%8A%B8/dashboard

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -2,6 +2,7 @@ package hello.hellospring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import hello.hellospring.service.MemberService;
 
@@ -13,5 +14,10 @@ public class MemberController {
 	@Autowired
 	public MemberController(MemberService memberService) {
 		this.memberService = memberService;
+	}
+
+	@GetMapping("/members/new")
+	public String CreateForm(){
+		return "members/createMemberForm";
 	}
 }
