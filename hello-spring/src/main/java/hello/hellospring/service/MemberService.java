01192ec9ca6d3a020a37367cef540bb0562f85fc
hb9397 <hb9397@naver.com>
2023-08-10 23:59:15 +0900

    스프링입문_Section6_스프링 DB접근 기술 1. H2 DB 설치 2. 순수 JDBC 3. 스프링 통합 테스트 4. 스프링 JdbcTemplate
    - Test 코드가 현재 Spring 버전을 잘못 설정하여 실행되지 않아 코드 작성은 하지 않았음
    
    https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81-%EC%9E%85%EB%AC%B8-%EC%8A%A4%ED%94%84%EB%A7%81%EB%B6%80%ED%8A%B8/dashboard

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,6 +3,8 @@ package hello.hellospring.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,7 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 
 //@Service
+@Transactional
 public class MemberService {
 	private final MemberRepository memberRepository;
 
