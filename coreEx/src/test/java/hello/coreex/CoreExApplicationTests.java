3771a69e7de0f05c424be5ac0451c5e49c6a4665
hb9397 <hb9397@naver.com>
2023-08-16 18:36:19 +0900

    스프링핵심원리_기본편_Section2<예제 만들기>
    1. 프로젝트 생성

---
+++ coreEx/src/test/java/hello/coreex/CoreExApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.coreex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
