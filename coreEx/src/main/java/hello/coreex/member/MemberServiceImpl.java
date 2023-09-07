263180f0cb897e2dc307de4658d9f9f13f74e7be
hb9397 <hb9397@naver.com>
2023-09-07 17:28:51 +0900

    스프링핵심원리_기본편_Section6<컴포넌트 스캔> 1. 컴포넌트 스캔과 의존관계 자동 주입 시작하기(-ing)

--- coreEx/src/main/java/hello/coreex/member/MemberServiceImpl.java
+++ coreEx/src/main/java/hello/coreex/member/MemberServiceImpl.java
@@ -14,7 +14,7 @@ public class MemberServiceImpl implements MemberService{
 	*/
 	private final MemberRepository memberRepository;
 
-	@Autowired
+	@Autowired // --> applicationContext.getBean(MemberRepository.class) 와 유사한 역할
 	// ComponentScan 사용 이전에 AppConfig 에서는 @Bean 으로 직접 설정 정보를 작성했고, 의존관계도 직접 명시했다.
 	// 이제는 이런 설정 정보 자체가 없기 때문에, 의존관계 주입도 이 클래스 안에서 해결해야 한다.
 	// @Autowired 는 의존관계를 자동으로 주입해준다.
